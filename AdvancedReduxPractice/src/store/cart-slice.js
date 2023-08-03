6eac9c14c930c9c5461870d8ba6f234e29921837
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2023-08-03 15:12:41 +0500

    Using useEffect with Redux Toolkit and handling http status feedback with RTK

--- AdvancedReduxPractice/src/store/cart-slice.js
+++ AdvancedReduxPractice/src/store/cart-slice.js
@@ -40,6 +40,12 @@ const cartSlice = createSlice({
   },
 });
 
+const sendCartData = (cartData) => {
+  return (dispatch) => {
+    dispatch();
+  };
+};
+
 export const cartActions = cartSlice.actions;
 
 export default cartSlice.reducer;
--- AdvancedReduxPractice/src/store/ui-slice.js
+++ AdvancedReduxPractice/src/store/ui-slice.js
@@ -11,6 +11,13 @@ const uiSlice = createSlice({
     toggleCartVisiblity(state) {
       state.cartIsVisible = !state.cartIsVisible;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
   },
 });
 
