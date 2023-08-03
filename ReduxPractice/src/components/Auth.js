093d355e9ee49686206db0e2fbbed9e76cb923e2
Ahsan-Masood98 <ahsanmasood795@gmail.com>
2023-08-03 10:58:05 +0500

    Working with Multiple Slices & Reading And Dispathing from New Slice

--- ReduxPractice/src/components/Auth.js
+++ ReduxPractice/src/components/Auth.js
@@ -1,22 +1,31 @@
-import classes from './Auth.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./Auth.module.css";
+import { authActions } from "../store";
 
 const Auth = () => {
+  const dispatch = useDispatch();
+
+  const authHandler = () => {
+    dispatch(authActions.login());
+  };
   return (
-    <main className={classes.auth}>
-      <section>
-        <form>
-          <div className={classes.control}>
-            <label htmlFor='email'>Email</label>
-            <input type='email' id='email' />
-          </div>
-          <div className={classes.control}>
-            <label htmlFor='password'>Password</label>
-            <input type='password' id='password' />
-          </div>
-          <button>Login</button>
-        </form>
-      </section>
-    </main>
+    <>
+      <main className={classes.auth}>
+        <section>
+          <form>
+            <div className={classes.control}>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <button onClick={authHandler}>Login</button>
+          </form>
+        </section>
+      </main>
+    </>
   );
 };
 
